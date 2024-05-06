ef58c31d1cfe151c2a88921f626056b74b05233c
Wamitinewton <wamitinewton@gmail.com>
2024-05-06 23:19:10 +0300

    Android

---
+++ android/app/src/main/kotlin/com/example/android_app1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.android_app1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/on_boarding.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingWelcomeScreen extends StatelessWidget {
+  const OnBoardingWelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(
+                color: Colors.black,
+                image: DecorationImage(
+                    image: AssetImage('assets/onBoarding.png'),
+                    fit: BoxFit.cover)),
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 250),
+                    child: Text(
+                      'Make Your',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "HOME BEAUTIFUL",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: Colors.black),
+                  ),
+                  const Spacer(flex: 2,),
+                  Container(
+                    height: 55,
+                    width: double.infinity,
+                    margin: const EdgeInsets.symmetric(horizontal: 80),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Get started',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          elevation: 4,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          )),
+                    ),
+                  ),
+                  Spacer(flex: 2,),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
